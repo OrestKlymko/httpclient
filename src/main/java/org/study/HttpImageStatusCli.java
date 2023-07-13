7193835dfc21c884f6e1e71c6f30f238c23e9883
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-07-13 16:57:14 +0200

    Update HttpImageStatusCli.java

--- src/main/java/org/study/HttpImageStatusCli.java
+++ src/main/java/org/study/HttpImageStatusCli.java
@@ -3,18 +3,20 @@ package org.study;
 import java.util.Scanner;
 
 public class HttpImageStatusCli {
-	public void askStatus(){
-
+		public void askStatus() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter HTTP status code: ");
-		int inputCode = scanner.nextInt();
-		scanner.close();
-		try {
-			String statusImage = new HttpStatusChecker().getStatusImage(inputCode);
-			new HttpStatusImageDownloader().downloadStatusImage(inputCode);
-		} catch (Exception e) {
-			throw new RuntimeException("There is not image for HTTP status "+inputCode);
-		}
+		int inputCode = 0;
 
+		try {
+			System.out.print("Enter HTTP status code: ");
+			inputCode = scanner.nextInt();
+			scanner.close();
+			new HttpStatusChecker().getStatusImage(inputCode);
+			new HttpStatusImageDownloader().downloadStatusImage(inputCode);
+		} catch (InputMismatchException e) {
+			System.out.println("Enter valid code");
+		} catch (Exception e) {
+			throw new RuntimeException("There is not image for HTTP status " + inputCode);
+		}
 	}
 }
